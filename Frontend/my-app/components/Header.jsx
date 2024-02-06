ca5762ba4c1d03c3a50a4095c04a3fc1a66ecbfc
SACHIN NIRATE <96587569+CSEcoolgamer@users.noreply.github.com>
2024-02-07 00:57:14 +0530

    Header updated

--- Frontend/my-app/components/Header.jsx
+++ Frontend/my-app/components/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Wrapper from "./Wrapper";
 
 const Header = () => {
+  
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState("translate-y-0");
+  const [lastScrollY, setLastScrollY] = useState(0);
+
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+      <header
+          className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 
+          transition-transform duration-300 ${show}`}
+      >
+        <Wrapper>
+          <img src="/logo.svg" className="w-[40px] md:w-[60px]" />
+        </Wrapper>
+      </header>
+  );
+};
 
 export default Header
