3f57f9f13b38d015b9edf322a00958667b7e2544
SACHIN NIRATE <96587569+CSEcoolgamer@users.noreply.github.com>
2024-02-29 22:49:03 +0530

    done

---
+++ Frontend/my-app/components/ProductCard.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+
+
+const ProductCard = () => {
+  return (
+    <Link 
+        href="/product/1"
+        className='transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer'>
+        <img className='w-full' src='/product-1.webp' alt='Product Image' />
+        <div className='p-4 text-black/[0.9]'>
+            <h2 className='text-lg font-medium'>Product Name</h2>
+            <div className='flex items-center text-black/[0.5]'>
+                <p className='mr-2 text-lg font-semibold'>$20.00</p>
+                <p className='text-base font-medium line-through'>$25.00</p>
+                <p className='ml-auto text-base font-medium text-green-500'>20% off</p>
+
+            </div>
+
+        </div>
+    </Link>
+  )
+}
+
+export default ProductCard
--- Frontend/my-app/pages/index.js
+++ Frontend/my-app/pages/index.js
@@ -1,12 +1,42 @@
 import HeroBanner from "@/components/HeroBanner";
+import ProductCard from "@/components/ProductCard";
+import Wrapper from "@/components/Wrapper";
 
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 
 export default function Home() {
   return (
-    <main className="h-[500px]">
+    <main>
       <HeroBanner />
+      <Wrapper>
+
+        {/* heading and paragaph start */}
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Cushioning for Your Miles
+          </div>
+          <div className="text-md md:text-xl">
+            A lightweight Nike ZoomX midsole is combined with increased stack
+            heights to help provide cushioning during extended stretches of
+            running.
+          </div>
+        </div>
+        {/* heading and paragaph end */}
+
+        {/* products grid start */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+        </div>
+        {/* products grid end */}
+
+      </Wrapper>
     </main>
   );
 }
